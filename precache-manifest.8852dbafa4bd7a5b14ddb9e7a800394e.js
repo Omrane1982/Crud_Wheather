9d001268f0cdd766e0379ae6221729c4963401ac
omrane1982 <omrane2011@gmail.com>
2019-06-28 08:03:11 +0100

    Updates

---
+++ precache-manifest.8852dbafa4bd7a5b14ddb9e7a800394e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c147eba9af2d15f9ee65c86d24a5df1",
+    "url": "/Crud_Wheather/index.html"
+  },
+  {
+    "revision": "c89ac8a42e6ffb6d7ed8",
+    "url": "/Crud_Wheather/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Crud_Wheather/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c89ac8a42e6ffb6d7ed8",
+    "url": "/Crud_Wheather/static/js/main.10a7f41b.chunk.js"
+  },
+  {
+    "revision": "732c38f5afcdf7c96126",
+    "url": "/Crud_Wheather/static/js/runtime~main.8fa55334.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Crud_Wheather/static/media/logo.5d5d9eef.svg"
+  }
+]);
